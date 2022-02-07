718ffa4eb7abbb79febd1b8678e971644169216b
nainilnandu <nainilnandu@gmail.com>
2022-02-07 18:18:53 +0530

    Nothing is Changed

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -17,7 +17,7 @@ export default function Homepage() {
     const count = await blogFactoryContract.methods.getBlogCount().call()
     console.log(count)
 
-    const blog = await blogFactoryContract.methods.getBlogFromBlogHash("31467967172501424478726818438933228576082631935286580039969369046696776371013").call()
+    const blog = await blogFactoryContract.methods.getBlogFromBlogHash("104242884546232587208185761615075562449322657461960659457127498426951089024742").call()
     console.log(blog)
 
     // const blog = await blogFactoryContract.methods.getBlog(0).call()
